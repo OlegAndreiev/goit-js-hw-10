2657ecd4d7c1b9edd9c9cae17a30ce22035330f6
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-10-15 17:35:08 +0300

    working

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,19 @@
-import { inputSearch } from './index';
+import Notiflix from 'notiflix';
+import {
+  inputSearch,
+  DEFAULT_URL,
+  countryInfo,
+  countryList,
+  createMarkupOneCountry,
+  createMarkupFewCountries,
+} from './index';
 
-const DEFAULT_URL = 'https://restcountries.com/v3.1/name';
-const countryInfo = document.querySelector('.country-info');
-export let outputData;
 export function fetchCountries(name) {
-  // console.dir(inputSearch);
   if (inputSearch.value !== '') {
     const input = inputSearch.value;
     const inputTrim = input.trim();
-    //   const resp = fetch(`${DEFAULT_URL}/${input}`);
     const resp = fetch(
-      `https://restcountries.com/v3.1/name/${inputTrim}?fields=name,capital,population,flags,languages`
+      `${DEFAULT_URL}/${inputTrim}?fields=name,capital,population,flags,languages`
     );
 
     resp
@@ -22,16 +25,13 @@ export function fetchCountries(name) {
       })
       .then(data => {
         if (data.length >= 10) {
-          // Notify.success(
-          //   `❌ Too many matches found. Please enter a more specific name.`
-          // );
-          alert(
-            `❌ Too many matches found. Please enter a more specific name.`
+          Notiflix.Notify.failure(
+            `Too many matches found. Please enter a more specific name.`
           );
         }
         if (data.length > 2 && data.length < 10) {
           const markupCountries = createMarkupFewCountries(data);
-          countryInfo.innerHTML = markupCountries;
+          countryList.innerHTML = markupCountries;
         }
         if (data.length === 1) {
           const markupCountry = createMarkupOneCountry(data);
@@ -40,40 +40,15 @@ export function fetchCountries(name) {
 
         console.log(data.length);
       })
-      .catch(err => alert(`❌ Oops, there is no country with that name.`));
+      .catch(err =>
+        Notiflix.Notify.failure(`Oops, there is no country with that name.`)
+      );
   }
   if (inputSearch.value === ' ') {
     inputSearch.value === '';
     return;
   }
+  countryList.innerHTML = '';
   countryInfo.innerHTML = '';
   return;
 }
-
-function createMarkupOneCountry(arr) {
-  return arr
-    .map(
-      item =>
-        `<li class='item'>
-        <h2>Country: <img src="${item.flags.svg}" alt="flag" width=30> ${
-          item.name.official
-        }</h2>
-        <p>Capital: ${item.capital}</p>
-        <p>Population: ${item.population}</p>
-        <p>Languages: ${Object.values(item.languages)}</p></li>`
-    )
-    .join('');
-}
-
-function createMarkupFewCountries(arr) {
-  return arr
-    .map(
-      item =>
-        `<li class='item'>
-        <h2><img src="${item.flags.svg}" alt="flag" width=40 height=30> ${item.name.official}</h2>
-        </li>`
-    )
-    .join('');
-}
-
-// console.dir(inputSearch);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import './css/styles.css';
 const debounce = require('lodash.debounce');
-import Notiflix from 'notiflix';
-import { fetchCountries, data } from './fetchCountries';
-import { outputData } from './fetchCountries';
+
+import { fetchCountries } from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
+export const DEFAULT_URL = 'https://restcountries.com/v3.1/name';
 
-// const DEFAULT_URL = 'https://restcountries.com/v3.1/name/';
-
-// fetch(`${DEFAULT_URL}/Ukraine`);
 export const inputSearch = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-// console.log(countryInfo);
+export const countryList = document.querySelector('.country-list');
+export const countryInfo = document.querySelector('.country-info');
 
 inputSearch.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+export function createMarkupOneCountry(arr) {
+  return arr
+    .map(
+      item =>
+        `<div class='item-country'><img src="${
+          item.flags.svg
+        }" alt="flag" width=60 height=50>
+        <h2 class='title'> ${item.name.official}</h2></div>
+        
+        <p><span>Capital:</span> ${item.capital}</p>
+        <p><span>Population:</span> ${item.population}</p>
+        <p><span>Languages:</span> ${Object.values(item.languages)}</p>
+       <p><img src="https://www.svgrepo.com/show/306970/wikipedia.svg" alt="Wikipedia" width=30> <a href="https://uk.wikipedia.org/wiki/${
+         item.name.official
+       } " target="_blank">Link to Wikipedia</a></p>
+       <p><img src="https://www.svgrepo.com/show/353819/google-maps.svg" alt="Google maps" width=30><a href="https://www.google.com.ua/maps/place/${
+         item.name.official
+       }"target="_blank">Link to Google maps</a></p>`
+    )
+    .join('');
+}
+
+export function createMarkupFewCountries(arr) {
+  return arr
+    .map(
+      item =>
+        `<li class='item'>
+        <img src="${item.flags.svg}" alt="flag" width=40 height=30>
+        <h2> ${item.name.official}</h2>
+        </li>`
+    )
+    .join('');
+}
